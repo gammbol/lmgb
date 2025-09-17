01efd0a3df09df9115322eb060112b7a6091a46e
tryingtothink <gammbol2525@gmail.com>
2025-09-17 14:56:28 +0300

    dev mbc b4 reconstruction

---
+++ src/mbc/mbc.cpp
@@ -0,0 +1,3 @@
+#include "mbc.h"
+
+lmgb::mbc::mbc() { selectedBank = mem.Read(0x0147); }
--- src/mbc/mbc.h
+++ src/mbc/mbc.h
@@ -1,12 +1,20 @@
 #ifndef LMGB_MBC_H
 #define LMGB_MBC_H
 
+#include "mem.h"
+
 namespace lmgb {
 
 const int ROMBankSize = 0x4000;
 const int RAMBankSize = 0x2000;
 
-class mbc {};
+class mbc {
+  char type;
+  char selectedBank;
+
+public:
+  mbc();
+};
 } // namespace lmgb
 
 #endif
